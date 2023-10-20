a34869561983c8168595fe577a17b1bdb0a2c9b1
mocha-06 <mocha.isa.0625@gmail.com>
2023-10-20 12:54:25 +0900

    📝 コメント追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,21 @@ import { CsrfToken } from './types'
 
 function App() {
   useEffect(() => {
+    // Cookieと認証情報をサーバーに送信し、ユーザーを認証できるように
     axios.defaults.withCredentials = true
     const getCsrfToken = async () => {
+      // {data}にAPIから取得できたCSRFトークンを代入
       const { data } = await axios.get<CsrfToken>(
         `${process.env.REACT_APP_API_URL}/csrf`
       )
+      // 全てのHTTPリクエストヘッダーにX-CSRF-Tokenを追加
+      // このtoken(上記で取得した)の有無でリクエストが有効かを判断
       axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
     }
     getCsrfToken()
   }, [])
   return (
+    // URLパスの監視や、ラウザの履歴管理のために必要
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Auth />} />
--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,26 @@ import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+// データのキャッシュやデータの非同期取得に使用
 const queryClient = new QueryClient({})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
+  // ReactのStrictモードを有効に、潜在的な問題を検出し、警告を表示。問題を特定しやすく
   <React.StrictMode>
+    {/* React Queryを利用できるようにする */}
     <QueryClientProvider client={queryClient}>
     <App />
+    {/* Devtoolsパネルを非表示に、必要に応じて表示できる */}
     <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+// パフォーマンスを測定し、分析、監視するために使用
