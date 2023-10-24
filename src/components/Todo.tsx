9518e604030ca8c613c55db9ce5b26e2c2160521
mocha-06 <mocha.isa.0625@gmail.com>
2023-10-24 11:33:44 +0900

    🐛 記述ミス

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -32,27 +32,27 @@ export const Todo = () => {
     queryClient.removeQueries(['tasks'])
   }
   return (
-    <div className='flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono'>
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
       <div className="flex items-center my-3">
-        <ShieldCheckIcon className='h-8 w-8 mr-3 text-indigo-500 cursor-pointer' />
-        <span className='text-center text-3xl font-extrabold'>
+        <ShieldCheckIcon className="h-8 w-8 mr-3 text-indigo-500 cursor-pointer" />
+        <span className="text-center text-3xl font-extrabold">
           Task Manager
         </span>
       </div>
       <ArrowRightOnRectangleIcon
         onClick={logout}
-        className='h-6 w-6 my-6 text-blue-500 cursor-pointer'
+        className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
       />
       <form onSubmit={submitTaskHandler}>
         <input
-          className='mb-3 mr-3 px-3 py-2 border border-gray-300'
+          className="mb-3 mr-3 px-3 py-2 border border-gray-300"
           placeholder="title ?"
           type="text"
           onChange={(e) => updateTask({ ...editedTask, title: e.target.value })}
           value={editedTask.title || ''}
         />
         <button
-          className='disvbled:opacity-40 mx-3 py-2 px-3 text-white bg-indigo-600 rounded'
+          className="disabled:opacity-40 mx-3 py-2 px-3 text-white bg-indigo-600 rounded"
           disabled={!editedTask.title}
         >
           {editedTask.id === 0 ? 'Create' : 'Update'}
